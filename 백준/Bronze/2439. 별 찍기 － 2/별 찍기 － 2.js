875076d2f166bc9bdbbce9120cceb6c69d7933f0
Hongwon Lim <w___on@naver.com>
2024-10-01 17:09:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 92 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const n = Number(input);
+
+for(let i = 1; i <= n; i++) {
+  console.log(' '.repeat(n - i) + '*'.repeat(i));
+}
